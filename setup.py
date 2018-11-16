8a702c8b74a24deb2c93df08349c0b40a7332d9b
andrewbrooks-o <44206483+andrewbrooks-o@users.noreply.github.com>
2018-11-16 15:00:56 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bratreader',
+    version='0.0',
+    author='Andrew Brooks',
+    description='installable version of clips/brat-reader',
+    packages=find_packages(),
+    python_requires='>=2.7',
+    license='MIT',
+)
